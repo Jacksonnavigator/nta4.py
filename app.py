8d707688cdd418f2822bc67346f00d107eb616f7
Jacksonnavigator <135830277+Jacksonnavigator@users.noreply.github.com>
2023-06-16 01:30:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ number = random.randint(1, 100)
 
 guess = st.number_input("Enter a number (between 1 and 100):", min_value=1,max_value=100)
 
-if st.button('Make a guess!'):
+if st.button('Make a guess!');
     if guess > number:
         st.write('Too high! Try a smaller number.')
     elif guess < number:
